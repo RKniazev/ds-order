c755c0b6cee09a05da472ff77fb3544f6e9ac8c3
Roman Kniazev <kniazevroman@gmail.com>
2019-08-18 23:50:00 +0300

    first MVP

--- content.js
+++ content.js
@@ -5,9 +5,11 @@ document.querySelector("#top-links .list-inline").prepend(li);
 
 let divForm = document.createElement("div");
 
-divForm.innerHTML = '<div id="divForm" class="hidden" style="width: 100%;height: 100%;background-color: rgba(0,0,0,0.8);overflow:hidden;position:fixed;top:0px;z-index: 999;text-align: center;padding: 25px 50px;"><div><span style="font-size: 48px;height: 100%;">Заказ:</span> <form><textarea type="text" name="order" style="width: 65%;margin-top: 20px;padding: 10px;margin-bottom: 0px;background-color: white !important;color: black !important;" rows="20"></textarea><br></form><a id="btnForm" style="background-color: #ffd600;border: none;padding: 15px 32px;text-align: center;text-decoration: none;font-size: 16px;text-transform: uppercase;font-weight: 900;height: 50px;margin-top: 20px;color: black;display: inline-block;width: 150px;">GO</a></div></div>';
+divForm.innerHTML = '<div id="divForm" class="hidden" style="width: 100%;height: 100%;background-color: rgba(0,0,0,0.8);overflow:hidden;position:fixed;top:0px;z-index: 999;text-align: center;padding: 25px 50px;"><div><span style="font-size: 48px;height: 100%;">Заказ:</span> <form><textarea id="orderText" type="text" name="order" style="width: 65%;margin-top: 20px;padding: 10px;margin-bottom: 0px;background-color: white !important;color: black !important;" rows="20"></textarea><br></form><a id="btnForm" style="background-color: #ffd600;border: none;padding: 15px 32px;text-align: center;text-decoration: none;font-size: 16px;text-transform: uppercase;font-weight: 900;height: 50px;margin-top: 20px;color: black;display: inline-block;width: 150px;">GO</a></div></div>';
 
 document.querySelector('body').append(divForm);
+
+let arrayOfLines = ["1","2"];
 
 li.onclick = function() {
     let element = document.getElementById("divForm");
@@ -17,4 +19,28 @@ li.onclick = function() {
 btnForm.onclick = function() {
     let element = document.getElementById("divForm");
     element.classList.add("hidden");
+    let textArea = document.getElementById("orderText").value.split("\n");
+    let pageSku = document.querySelectorAll('#products-table tr');
+    console.log(textArea);
+    console.log(pageSku);
+
+    textArea.forEach(element => {
+        let find = false;
+        
+        if (element.length >= 2)
+        {
+            let inpSku = element.split("	")[0];
+            let value = element.split("	")[1];
+            
+            pageSku.forEach(elmtSku => {
+                let sku = elmtSku.querySelector('td:nth-child(2)').innerText;
+                
+                if (find == false && inpSku == sku) {
+                    find = true;
+                    console.log('Нашли' + sku + ' / заполняем ' + value);
+                    elmtSku.querySelector('td:nth-child(5) > div > input').value = parseInt(value);
+                }
+            });
+        }
+    });
 }
